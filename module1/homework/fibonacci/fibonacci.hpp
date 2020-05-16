8495f4e2871acd1cac9c163d520e49e4d8b02093
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-05-17 00:37:42 +0200

    MOD: condition check for value less or equal 1 changed in iterative version

--- module1/homework/fibonacci/fibonacci.hpp
+++ module1/homework/fibonacci/fibonacci.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-int fibonacci_iterative(int sequence) {
+int fibonacci_iterative(int sequence) { 
+    if (sequence <= 1) {
+        return sequence;
+    }
     
-    if (sequence == 0) return 0;
-    if (sequence == 1) return 1;
-
     int current = 0;
     int previous = 1;
     int prev_previous = 0;
@@ -20,7 +20,8 @@ int fibonacci_iterative(int sequence) {
 }
 
 int fibonacci_recursive(int sequence) {
-    if (sequence == 0) return 0;
-    if (sequence == 1) return 1;
+    if (sequence <= 1) {
+        return sequence;
+    }
     return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
 }
