3d950998ffa3267cce6a1f27afb5b93ec3bcade9
lisie-sprawy <magda.jankowska6@tlen.pl>
2020-05-24 23:59:19 +0200

    adding nwdNWW fixed

--- module2/homework/nwdNww/nwdNww.hpp
+++ module2/homework/nwdNww/nwdNww.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-   if (rhs!=0)
-        return NWD (rhs,lhs%rhs);
-        return std::abs(lhs); 
+   if (rhs != 0){
+        return NWD (rhs, lhs%rhs);
+   }
+    return std::abs(lhs); 
 }
 
 int NWW(int lhs, int rhs) {
-    if (lhs==0 || rhs==0)
+    if (lhs==0 || rhs==0){
         return 0;
-        return std::abs(lhs * rhs)/NWD(lhs,rhs);
+    }
+    return std::abs(lhs * rhs) / NWD(lhs,rhs);
 }
