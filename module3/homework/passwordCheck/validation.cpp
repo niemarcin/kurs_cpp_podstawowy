dfeb47f4b82b071dc293cf8d6cc7d1891ec5cf34
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-05-31 23:53:05 +0200

    getErrorMessage function implemented

--- module3/homework/passwordCheck/validation.cpp
+++ module3/homework/passwordCheck/validation.cpp
@@ -1,2 +1,30 @@
 #include "validation.hpp"
-// TODO: Put implementations here
+
+std::string getErrorMessage(ErrorCode error) {
+    std::string errorMessage;
+    
+    switch (error) {
+        case ErrorCode::Ok :
+            errorMessage = "OK";
+        break;
+        case ErrorCode::PasswordNeedsAtLeastNineCharacters :
+            errorMessage = "Password needs at least nine characters";
+        break;
+        case ErrorCode::PasswordNeedsAtLeastOneNumber :
+            errorMessage = "Password needs at least one number";
+        break;
+        case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter :
+            errorMessage = "Password needs at least one special character";
+        break;
+        case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter :
+            errorMessage = "Password needs at least one uppercase letter";
+        break;
+        case ErrorCode::PasswordsDoesNotMatch :
+            errorMessage = "Passowrds does not match";
+        break;
+        default:
+            errorMessage = "Unknown error code";
+        break; 
+    }
+    return errorMessage;
+}
--- module3/homework/passwordCheck/validation.hpp
+++ module3/homework/passwordCheck/validation.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 
 enum class ErrorCode {
     Ok,
@@ -8,3 +9,5 @@ enum class ErrorCode {
     PasswordNeedsAtLeastOneUppercaseLetter,
     PasswordsDoesNotMatch
 };
+
+std::string getErrorMessage(ErrorCode error);
