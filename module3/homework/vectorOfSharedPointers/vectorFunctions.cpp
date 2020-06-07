6a2a8e3b5ab3b401697f869f6a022998e1290f16
Marcin N <19634288+niemarcin@users.noreply.github.com>
2020-06-07 12:35:44 +0200

    clean code

--- module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
+++ module3/homework/vectorOfSharedPointers/vectorFunctions.cpp
@@ -4,7 +4,7 @@
 std::vector<std::shared_ptr<int>> generate(int count){
     std::vector<std::shared_ptr<int>> newVector;
     newVector.reserve(count);
-    for (size_t i = 0 ; i < count ; ++i) {
+    for (size_t i = 0; i < count; ++i){
         newVector.emplace_back(std::make_shared<int>(i));
     }
     return newVector;
@@ -13,25 +13,25 @@ std::vector<std::shared_ptr<int>> generate(int count){
 void print(const std::vector<std::shared_ptr<int>>& newVector){
     for (const auto& element : newVector){
         std::cout << *element << "\n";
-  }
+    }
 }
 
 void add10(const std::vector<std::shared_ptr<int>>& newVector){
-  for (const auto& element : newVector){
-    if (element != nullptr){
-     *element += 10;
+    for (const auto& element : newVector){
+        if (element != nullptr){
+            *element += 10;
+        }
     }
-  }
 }
 
 void sub10(int* const ptr){
-  if (ptr != nullptr){
-    *ptr -= 10;
-  }
+    if (ptr != nullptr){
+        *ptr -= 10;
+    }
 }
 
-void sub10 (const std::vector<std::shared_ptr<int>>& newVector){
-  for (const auto& element : newVector){
-    sub10 (element.get());
-  }
+void sub10(const std::vector<std::shared_ptr<int>>& newVector){
+    for (const auto& element : newVector){
+        sub10(element.get());
+    }
 }
